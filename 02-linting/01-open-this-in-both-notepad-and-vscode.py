5543b3cb744749160fb68dfcd81a703256de1353
Peter Seale <peter@pseale.com>
2022-06-15 18:08:15 -0500

    update demos

--- 02-linting/01-open-this-in-both-notepad-and-vscode.py
+++ 02-linting/01-open-this-in-both-notepad-and-vscode.py
@@ -1,4 +1,4 @@
-for i in range(1,100 + 1):
+for i in range(1, 100 + 1):
     if i % 5 == 0 and i % 3 == 0:
         print("fizzbuzz")
     elif i % 3 == 0:
@@ -6,4 +6,4 @@ for i in range(1,100 + 1):
     elif i % 5 == 0:
         print("buzz")
     else:
-        print(i)
+        primt(i)
